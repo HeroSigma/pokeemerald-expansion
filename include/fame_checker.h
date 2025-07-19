fba2c7c7d4503eab786191644cb3da4751afa5ab
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-19 11:29:14 -0500

    Add Fame Checker constants

--- include/fame_checker.h
+++ include/fame_checker.h
@@ -1,6 +1,23 @@
 #ifndef GUARD_FAME_CHECKER_H
 #define GUARD_FAME_CHECKER_H
 
-void CB2_OpenFameChecker(void);
+#include "main.h"
+#include "constants/fame_checker.h"
 
-#endif // GUARD_FAME_CHECKER_H
+enum {
+    FCWINDOWID_LIST,
+    FCWINDOWID_UIHELP,
+    FCWINDOWID_MSGBOX,
+    FCWINDOWID_ICONDESC
+};
+
+extern struct ListMenuTemplate gFameChecker_ListMenuTemplate;
+extern u8 gIconDescriptionBoxIsOpen;
+
+void ResetFameChecker(void);
+void FullyUnlockFameChecker(void);
+void UseFameChecker(MainCallback savedCallback);
+void SetFlavorTextFlagFromSpecialVars(void);
+void UpdatePickStateFromSpecialVar8005(void);
+
+#endif //GUARD_FAME_CHECKER_H
