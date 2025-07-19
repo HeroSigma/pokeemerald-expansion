91228ef20a8140243d8519b4692b0416b61e7224
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-19 11:16:29 -0500

    Add stub Fame Checker feature

---
+++ include/fame_checker.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_FAME_CHECKER_H
+#define GUARD_FAME_CHECKER_H
+
+void CB2_OpenFameChecker(void);
+
+#endif // GUARD_FAME_CHECKER_H
---
+++ src/fame_checker.c
@@ -0,0 +1,9 @@
+#include "global.h"
+#include "fame_checker.h"
+#include "main.h"
+
+void CB2_OpenFameChecker(void)
+{
+    // Placeholder for fame checker feature
+    SetMainCallback2(CB2_ReturnToFieldContinueScriptPlayMapMusic);
+}
